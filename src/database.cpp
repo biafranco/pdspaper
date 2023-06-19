50893b9ea9cede7c4490b1c77a141856e18cf168
IsaCunha <isadorawt@gmail.com>
2023-06-19 16:59:14 -0300

    queria que git fosse menos confuso

---
+++ src/database.cpp
@@ -0,0 +1,13 @@
+#include "database.h"
+#include <fstream>
+
+using namespace std;
+
+Database::Database(string path){}
+
+string Database::normalize(string input) {}
+
+void Database::pesquisa(vector<string> palavras){}
+
+Database::~Database(){
+}
