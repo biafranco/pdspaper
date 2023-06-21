1ef1275f2e7cf2889ee53d92920c2d547bd14623
Bianca Franco <biancagabr@gmail.com>
2023-06-21 03:55:45 -0300

    compilando + primeiros testes

--- include/database.h
+++ include/database.h
@@ -1,43 +1,43 @@
 #ifndef DATABASE_H_
 #define DATABASE_H_
 
-#include <iostream>
 #include <cstring>
-#include <map>
-#include <set>
 #include <dirent.h>
 #include <fstream>
-#include <vector>
+#include <iostream>
+#include <map>
 #include <queue>
+#include <set>
+#include <vector>
 
 using namespace std;
 
 struct ComparaPar {
-    bool operator()(const pair<string, int>& p1, const pair<string, int>& p2) const {
-        if (p1.second != p2.second) {
-            return p1.second < p2.second; 
-        }
-        return p1.first > p2.first; 
+  bool operator()(const pair<string, int> &p1,
+                  const pair<string, int> &p2) const {
+    if (p1.second != p2.second) {
+      return p1.second < p2.second;
     }
+    return p1.first > p2.first;
+  }
 };
 
 class Database {
 public:
-    Database(string path);
+  Database(string path);
 
-    void ProcessTextFile(const string& arquivo);
+  void ProcessTextFile(const string path, const string &nomeArquivo);
 
-    string Normaliza(string input);
+  string Normaliza(string input);
 
-    void Pesquisa(const vector<string>& palavras);
+  void Pesquisa(const vector<string> &palavras);
 
-    int pegaIndicePalRep(const string& palavra, const string& arquivo);
+  int pegaIndicePalRep(const string &palavra, const string &nomeArquivo);
 
-    ~Database();
-
+  ~Database();
 
 private:
-   map<string, map<string, int>> _indice;
+  map<string, map<string, int>> _indice;
 };
 
 #endif
--- src/database.cpp
+++ src/database.cpp
@@ -1,132 +1,131 @@
 #include "../include/database.h"
-#include <fstream>
-#include <string>
-#include <cstring>
-#include <iostream>
-#include <dirent.h>
-#include <sstream>
 #include <algorithm>
+#include <cstring>
+#include <dirent.h>
+#include <fstream>
+#include <iostream>
+#include <locale>
+#include <regex>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
 Database::Database(string path) {
-   // DIR é uma estrutura que representa um diretório em um sistema de 
-   // arquivos. Ele contém informações sobre o diretório, como o descritor 
-   // de arquivo e a posição atual.
+  DIR *directory = opendir(path.c_str());
 
-   // dirent é uma estrutura que representa uma entrada de diretório, 
-   // ou seja, um arquivo ou diretório individual dentro do diretório 
-   // principal. Ela contém informações sobre o nome do arquivo, tipo de 
-   // arquivo, tamanho, entre outras propriedades.
-    DIR* pasta = opendir(path.c_str());
-        
-    if (pasta == nullptr) {
-        return;
+  if (directory == nullptr) {
+    cerr << "Erro ao abrir o diretório: " << path << endl;
+    return;
+  }
+
+  struct dirent *entry;
+
+  while ((entry = readdir(directory)) != nullptr) {
+    if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+      continue;
     }
 
-    struct dirent *entrada;
-        
-    while ((entrada = readdir(pasta)) != nullptr) {
-        // Ignora os diretórios pai e atual
-        if (strcmp(entrada->d_name, ".") == 0 || strcmp(entrada->d_name, "..") == 0) {
-            continue;
-        }
+    string nomeArquivo = entry->d_name;
+    ProcessTextFile(path, nomeArquivo);
+  }
 
-        string arquivo = path + "/" + entrada->d_name;
-        ProcessTextFile(arquivo);
-    }
-    
-    // Fecha o diretório
-    closedir(pasta);
+  closedir(directory);
 }
 
-void Database::ProcessTextFile(const string& arquivo) {
-    ifstream file(arquivo);
-    if (file.is_open()) {
-        string line;
-        int documentoID = 1;
+void Database::ProcessTextFile(const string path, const string &nomeArquivo) {
 
-        while (getline(file, line)) {
-            string textoNormalizado = Normaliza(line);
+  string caminhoArquivo = path + "/" + nomeArquivo;
+  ifstream file(caminhoArquivo);
+  if (file.is_open()) {
+    string line;
+    int documentoID = 1;
+    while (getline(file, line)) {
+      string textoNormalizado = Normaliza(line);
 
-            vector<string> palavras;
-            istringstream iss(textoNormalizado);
-            string palavra;
+      vector<string> palavras;
+      istringstream iss(textoNormalizado);
+      string palavra;
 
-            while (iss >> palavra) {
-                _indice[palavra][arquivo]++;  // Incrementa a contagem da palavra no arquivo
-            }
+      while (iss >> palavra) {
+        _indice[palavra]
+               [nomeArquivo]++; // Incrementa a contagem da palavra no arquivo
+      }
 
-            documentoID++;
-        }
-        // Remover o arquivo original
-        remove(arquivo.c_str());
-
-        // Renomear o arquivo temporário para o nome original
-        rename("temp.txt", arquivo.c_str());
-
-        file.close();   
-    } else {
-        cerr << "Erro ao abrir o arquivo: " << arquivo << endl;
+      documentoID++;
     }
+
+    file.close();
+  } else {
+    cerr << "Erro ao abrir o arquivo: " << nomeArquivo << endl;
+  }
 }
 
+string RemoveAcentos(const string &palavra) {
+  char comAcentos[] = "ÄÅÁÂÀÃäáâàãÉÊËÈéêëèÍÎÏÌíîïìÖÓÔÒÕöóôòõÜÚÛüúûùÇç";
+  char semAcentos[] = "AAAAAAaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUuuuuCc";
+
+  // Substitui os caracteres acentuados por seus equivalentes sem acento
+  for (int k = 0; k < strlen(palavra); k++) {
+    for (int i = 0; i < strlen(comAcentos); i++) {
+      if (palavra[k] == comAcentos[i]) {
+        palavra[k] = semAcentos[i];
+      }
+    }
+  }
+
+  return palavraSemAcentos;
+}
 
 string Database::Normaliza(string texto) {
-    //ASCII 65-90 Maiusculas
-    //ASCII 97-122 Minusculas
-    //ASCII 32 SPACE
-    string normalizada;
-    
-    for (char c: texto) {
-        //registra alfabeto minusculo e espaco simples
-        if ((c >= 97 && c <=122) || c == 32) {
-            normalizada += c;
-        }
-        else if (c >= 65 && c <= 90) {
-        //transforma maiuscula em minuscula
-            normalizada += tolower(c); 
-        }
-        //transforma novas linhas em espaco simples
-        else if (c == 10) {
-            c = 32;
-            normalizada += c;
-        } //qualquer outro caractere eh ignorado
+  string normalizada;
+  string noAcentos;
+
+  // noAcentos = RemoveAcentos(texto)
+
+  for (char c : texto) {
+    if ((c >= 97 && c <= 122) || c == 32) {
+      normalizada += c;
+    } else if (c >= 65 && c <= 90) {
+      normalizada += tolower(c);
+    } else if (c == 10) {
+      c = 32;
+      normalizada += c;
     }
-    
-    return normalizada;
+  }
+
+  return normalizada;
 }
 
-void Database::Pesquisa(const vector<string>& palavras) {
+void Database::Pesquisa(const vector<string> &palavras) {
+  for (const auto &palavra : palavras) {
 
-  for (const auto& palavra : palavras) {
-    
-        cout << palavra << ":" << endl;
-        int _a = 0;
-        if(_indice[palavra].size()){
-          for (const auto& entry : _indice[palavra]) {
-            _a++;
-              int contador = pegaIndicePalRep(palavra, entry.first);
-              cout << "(" << entry.first << ", " << contador << ")";
-            if(_a >= _indice[palavra].size()) cout << endl;
-            else cout << ", ";
-          }
-        }else{
-          cout << "Palavra não encontrada";
-        }
+    cout << palavra << ":" << endl;
+    int _a = 0;
+    if (_indice[palavra].size()) {
+      for (const auto &entry : _indice[palavra]) {
+        _a++;
+        int contador = pegaIndicePalRep(palavra, entry.first);
+        cout << "(" << entry.first << ", " << contador << ")";
+        if (_a >= _indice[palavra].size())
+          cout << endl;
+        else
+          cout << ", ";
+      }
+    } else {
+      cout << "Palavra não encontrada";
+    }
     _a = 0;
     cout << endl;
-    cout << "" << endl;
-    }
+  }
 }
 
-int Database::pegaIndicePalRep(const string& palavra, const string& arquivo) {
-    if (_indice.find(palavra) != _indice.end() && _indice[palavra].find(arquivo) != _indice[palavra].end()) {
-        return _indice[palavra][arquivo];
-    }
-    return 0;
+int Database::pegaIndicePalRep(const string &palavra, const string &arquivo) {
+  if (_indice.find(palavra) != _indice.end() &&
+      _indice[palavra].find(arquivo) != _indice[palavra].end()) {
+    return _indice[palavra][arquivo];
+  }
+  return 0;
 }
 
-
-Database::~Database() {
-}
+Database::~Database() {}
